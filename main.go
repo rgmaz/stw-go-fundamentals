9b6660d8885278457ad7419c1cbf576462953626
Ryan G. Mazambani <237030736+rgmaz@users.noreply.github.com>
2026-08-18 20:22:58 +0200

    Ponters in Go

--- main.go
+++ main.go
@@ -1,25 +1,14 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-type Point struct {
-	X, Y int
+func increment(n *int) {
+	*n++
 }
 
 func main() {
-	var x1, y1, x2, y2 int
-	fmt.Scan(&x1)
-	fmt.Scan(&y1)
-	fmt.Scan(&x2)
-	fmt.Scan(&y2)
-
-	p1 := Point{X: x1, Y: y1}
-	p2 := Point{X: x2, Y: y2}
-
-	dx := p2.X - p1.X
-	dy := p2.Y - p1.Y
-
-	fmt.Println(dx*dx + dy*dy)
+	var x int
+	fmt.Scan(&x)
+	increment(&x)
+	fmt.Println(x)
 }
