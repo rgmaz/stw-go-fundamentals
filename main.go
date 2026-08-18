816f627221a1a50d4aa606efea06eb94f4bd9d2b
Ryan G. Mazambani <237030736+rgmaz@users.noreply.github.com>
2026-08-18 15:02:43 +0200

    Formatted printing

--- main.go
+++ main.go
@@ -4,12 +4,17 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
 func main() {
 	r := bufio.NewReader(os.Stdin)
-	line, _ := r.ReadString('\n')
-	line = strings.TrimRight(line, "\r\n")
-	fmt.Println(strings.ToUpper(line))
+	name, _ := r.ReadString('\n')
+	name = strings.TrimRight(name, "\r\n")
+	ageStr, _ := r.ReadString('\n')
+	ageStr = strings.TrimRight(ageStr, "\r\n")
+	age, _ := strconv.Atoi(ageStr)
+
+	fmt.Printf("Hi, %s! You are %d years old.", name, age)
 }
