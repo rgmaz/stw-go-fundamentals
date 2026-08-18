84a680b289e28df7989bae4a611d155a389b9c5c
Ryan G. Mazambani <237030736+rgmaz@users.noreply.github.com>
2026-08-18 20:28:08 +0200

    Multiple Returns and the error Pattern

--- main.go
+++ main.go
@@ -1,14 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func increment(n *int) {
-	*n++
+func safeDivide(a, b int) (int, error) {
+	if b == 0 {
+		return 0, errors.New("divide by zero")
+	}
+	return a / b, nil
 }
 
 func main() {
-	var x int
-	fmt.Scan(&x)
-	increment(&x)
-	fmt.Println(x)
+	var a, b int
+	fmt.Scan(&a)
+	fmt.Scan(&b)
+	if q, err := safeDivide(a, b); err != nil {
+		fmt.Printf("error: %s\n", err)
+	} else {
+		fmt.Printf("result: %d\n", q)
+	}
 }
